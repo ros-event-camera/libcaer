5c931f30a3b6abcecf9b47a27e35d5923398b08d
Luca Longinotti <l@longi.li>
2016-08-25 20:16:02 +0200

    Thread support: remove Win32, we only support building under MinGW in Windows, due to other requirements like stdatomic.h that MSVC doesn't have, so we can just use its very good Pthreads emulation for now.

--- src/davis_common.h
+++ src/davis_common.h
@@ -9,9 +9,6 @@
 #ifdef HAVE_PTHREADS
 	#include "c11threads_posix.h"
 #endif
-#ifdef HAVE_WIN32_THREADS
-	#include "c11threads_win32.h"
-#endif
 
 #define APS_READOUT_TYPES_NUM 2
 #define APS_READOUT_RESET  0
