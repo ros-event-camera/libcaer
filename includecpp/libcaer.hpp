43e04ee11b4730cd4435257ff582913a15c02617
Luca Longinotti <l@longi.li>
2017-03-04 00:15:39 +0100

    C++: libcaer.hpp: fix warnings; add function prototypes and casts to uint8_t.

--- includecpp/libcaer.hpp
+++ includecpp/libcaer.hpp
@@ -18,8 +18,14 @@ enum class logLevel {
 	DEBUG = 7
 };
 
+void logLevelSet(logLevel l) noexcept;
+logLevel logLevelGet() noexcept;
+void fileDescriptorsSet(int fd1, int fd2) noexcept;
+void log(logLevel l, const char *subSystem, const char *format, ...) noexcept;
+void logVA(logLevel l, const char *subSystem, const char *format, va_list args) noexcept;
+
 void logLevelSet(logLevel l) noexcept {
-	caerLogLevelSet(static_cast<typename std::underlying_type<logLevel>::type>(l));
+	caerLogLevelSet(static_cast<uint8_t>(static_cast<typename std::underlying_type<logLevel>::type>(l)));
 }
 
 logLevel logLevelGet() noexcept {
@@ -33,12 +39,14 @@ void fileDescriptorsSet(int fd1, int fd2) noexcept {
 void log(logLevel l, const char *subSystem, const char *format, ...) noexcept {
 	va_list argumentList;
 	va_start(argumentList, format);
-	caerLogVA(static_cast<typename std::underlying_type<logLevel>::type>(l), subSystem, format, argumentList);
+	caerLogVA(static_cast<uint8_t>(static_cast<typename std::underlying_type<logLevel>::type>(l)), subSystem, format,
+		argumentList);
 	va_end(argumentList);
 }
 
 void logVA(logLevel l, const char *subSystem, const char *format, va_list args) noexcept {
-	caerLogVA(static_cast<typename std::underlying_type<logLevel>::type>(l), subSystem, format, args);
+	caerLogVA(static_cast<uint8_t>(static_cast<typename std::underlying_type<logLevel>::type>(l)), subSystem, format,
+		args);
 }
 
 }
