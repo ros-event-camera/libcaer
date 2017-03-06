af9cf8b60b99129c4e523faa11243a902dd0f777
Luca Longinotti <l@longi.li>
2017-03-06 04:43:06 +0100

    C++/Event::Utils: add useful functions for dealing with events. In this case, to properly construct typed EventPackets with the right constructor when making shared_ptrs.

---
+++ includecpp/events/utils.hpp
@@ -0,0 +1,90 @@
+#ifndef LIBCAER_EVENTS_UTILS_HPP_
+#define LIBCAER_EVENTS_UTILS_HPP_
+
+#include "common.hpp"
+#include "config.hpp"
+#include "ear.hpp"
+#include "frame.hpp"
+#include "imu6.hpp"
+#include "imu9.hpp"
+#include "point1d.hpp"
+#include "point2d.hpp"
+#include "point3d.hpp"
+#include "point4d.hpp"
+#include "polarity.hpp"
+#include "sample.hpp"
+#include "special.hpp"
+#include "spike.hpp"
+#include <memory>
+
+namespace libcaer {
+namespace events {
+namespace utils {
+
+std::shared_ptr<EventPacketHeader> constructFromCStruct(caerEventPacketHeader packet);
+
+std::shared_ptr<EventPacketHeader> constructFromCStruct(caerEventPacketHeader packet) {
+	switch (caerEventPacketHeaderGetEventType(packet)) {
+		case SPECIAL_EVENT:
+			return (std::make_shared<SpecialEventPacket>(packet));
+			break;
+
+		case POLARITY_EVENT:
+			return (std::make_shared<PolarityEventPacket>(packet));
+			break;
+
+		case FRAME_EVENT:
+			return (std::make_shared<FrameEventPacket>(packet));
+			break;
+
+		case IMU6_EVENT:
+			return (std::make_shared<IMU6EventPacket>(packet));
+			break;
+
+		case IMU9_EVENT:
+			return (std::make_shared<IMU9EventPacket>(packet));
+			break;
+
+		case SAMPLE_EVENT:
+			return (std::make_shared<SampleEventPacket>(packet));
+			break;
+
+		case EAR_EVENT:
+			return (std::make_shared<EarEventPacket>(packet));
+			break;
+
+		case CONFIG_EVENT:
+			return (std::make_shared<ConfigurationEventPacket>(packet));
+			break;
+
+		case POINT1D_EVENT:
+			return (std::make_shared<Point1DEventPacket>(packet));
+			break;
+
+		case POINT2D_EVENT:
+			return (std::make_shared<Point2DEventPacket>(packet));
+			break;
+
+		case POINT3D_EVENT:
+			return (std::make_shared<Point3DEventPacket>(packet));
+			break;
+
+		case POINT4D_EVENT:
+			return (std::make_shared<Point4DEventPacket>(packet));
+			break;
+
+		case SPIKE_EVENT:
+			return (std::make_shared<SpikeEventPacket>(packet));
+			break;
+
+		default:
+			return (std::make_shared<EventPacketHeader>(packet));
+			break;
+	}
+}
+
+}
+}
+}
+
+#endif /* LIBCAER_EVENTS_UTILS_HPP_ */
