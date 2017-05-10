178dbe9f02cf37679dcd93789e64698db250cd5a
Luca Longinotti <l@longi.li>
2017-05-10 13:52:36 +0200

    AutoExposure: use jAER algorithm to avoid under/overexposure.

--- src/autoexposure.c
+++ src/autoexposure.c
@@ -11,72 +11,133 @@ int32_t autoExposureCalculate(autoExposureState state, caerFrameEventConst frame
 		return (-1);
 	}
 
+	caerLog(CAER_LOG_DEBUG, "AutoExposure", "Last set exposure value was: %d.", exposureLastSetValue);
+	caerLog(CAER_LOG_DEBUG, "AutoExposure", "Frame exposure value was: %d.", exposureFrameValue);
+
 	int32_t frameSizeX = caerFrameEventGetLengthX(frame);
 	int32_t frameSizeY = caerFrameEventGetLengthY(frame);
 	const uint16_t *framePixels = caerFrameEventGetPixelArrayUnsafeConst(frame);
 
-	// Reset histogram.
-	for (size_t i = 0; i < AUTOEXPOSURE_MIDDLEGRAY_MSV; i++) {
+	// Reset histograms.
+	for (size_t i = 0; i < AUTOEXPOSURE_HISTOGRAM_PIXELS; i++) {
 		state->pixelHistogram[i] = 0;
 	}
+	for (size_t i = 0; i < AUTOEXPOSURE_HISTOGRAM_MSV; i++) {
+		state->msvHistogram[i] = 0;
+	}
 
-	// Fill histogram with 5 regions for MSV.
+	// Min/Max pixel values.
+	uint16_t minValue = UINT16_MAX;
+	uint16_t maxValue = 0;
+
+	// Fill histograms: 256 regions for pixel values; 5 regions for MSV.
 	for (int32_t y = 0; y < frameSizeY; y++) {
 		for (int32_t x = 0; x < frameSizeX; x++) {
 			uint16_t pixelValue = framePixels[(y * frameSizeX) + x];
 
-			size_t index = pixelValue / ((UINT16_MAX + 1) / AUTOEXPOSURE_MIDDLEGRAY_MSV);
+			// Track min/max pixel values.
+			if (pixelValue < minValue) {
+				minValue = pixelValue;
+			}
+			if (pixelValue > maxValue) {
+				maxValue = pixelValue;
+			}
 
-			state->pixelHistogram[index] += pixelValue;
+			// Update histograms.
+			size_t pixelIndex = pixelValue / ((UINT16_MAX + 1) / AUTOEXPOSURE_HISTOGRAM_PIXELS);
+			state->pixelHistogram[pixelIndex]++;
+
+			size_t msvIndex = pixelValue / ((UINT16_MAX + 1) / AUTOEXPOSURE_HISTOGRAM_MSV);
+			state->msvHistogram[msvIndex] += pixelValue;
 		}
 	}
 
-	// Histogram mean sample value.
+	// Calculate mean sample value from histogram.
 	float meanSampleValueNum = 0, meanSampleValueDenom = 0;
-	for (size_t i = 0; i < AUTOEXPOSURE_MIDDLEGRAY_MSV; i++) {
+	for (size_t i = 0; i < AUTOEXPOSURE_HISTOGRAM_MSV; i++) {
 		meanSampleValueNum += ((float) i + 1.0f) * (float) state->pixelHistogram[i];
 		meanSampleValueDenom += (float) state->pixelHistogram[i];
 	}
 
 	float meanSampleValue = meanSampleValueNum / meanSampleValueDenom;
-	float meanSampleValueError = (AUTOEXPOSURE_MIDDLEGRAY_MSV / 2.0f) - meanSampleValue;
+	float meanSampleValueError = (AUTOEXPOSURE_HISTOGRAM_MSV / 2.0f) - meanSampleValue;
 
 	caerLog(CAER_LOG_DEBUG, "AutoExposure", "Mean sample value error is: %f.", (double) meanSampleValueError);
-	caerLog(CAER_LOG_DEBUG, "AutoExposure", "Last set exposure value was: %d.", exposureLastSetValue);
-	caerLog(CAER_LOG_DEBUG, "AutoExposure", "Frame exposure value was: %d.", caerFrameEventGetExposureLength(frame));
 
-	if ((meanSampleValueError > 0.1f && state->previousError > 0
-		&& meanSampleValueError > (state->previousError + 0.001f))
-		|| (meanSampleValueError < -0.1f && state->previousError < 0
-			&& meanSampleValueError < (state->previousError - 0.001f))) {
-		// Error is getting worse and worse in same direction.
-		// Did we hit a local minima before? Stop now!
-		state->previousError = meanSampleValueError;
-		return (-1);
+	// Calculate statistics on pixel histogram.
+	size_t pixelsSum = 0;
+	size_t pixelsWeightedSum = 0;
+
+	for (size_t i = 0; i < AUTOEXPOSURE_HISTOGRAM_PIXELS; i++) {
+		size_t v = state->pixelHistogram[i];
+		pixelsSum += v;
+		pixelsWeightedSum += i * v;
+
+		if (v > 0) {
+			if (i > state->pixelsMaxNonZeroBin) {
+				state->pixelsMaxNonZeroBin = i;
+			}
+		}
 	}
+
+	size_t pixelsMeanBin = 0;
+
+	if (pixelsSum <= 0) {
+		pixelsMeanBin = AUTOEXPOSURE_HISTOGRAM_PIXELS / 2;
+	}
+	else {
+		pixelsMeanBin = pixelsWeightedSum / pixelsSum;
+	}
+
+	size_t pixels10Bin = (size_t) (AUTOEXPOSURE_LOW_BOUNDARY * (float) state->pixelsMaxNonZeroBin);
+	size_t pixels90Bin = (size_t) (AUTOEXPOSURE_HIGH_BOUNDARY * (float) state->pixelsMaxNonZeroBin);
+
+	float pixels10Sum = 0, pixels90Sum = 0;
+	float pixelsFracLow = 0, pixelsFracHigh = 0;
+
+	if (pixelsSum > 0) {
+		for (size_t i = 0; i <= pixels10Bin; i++) {
+			pixels10Sum += (float) state->pixelHistogram[i];
+		}
+
+		for (size_t i = pixels90Bin; i <= state->pixelsMaxNonZeroBin; i++) {
+			pixels90Sum += (float) state->pixelHistogram[i];
+		}
+
+		pixelsFracLow = pixels10Sum / (float) pixelsSum;
+		pixelsFracHigh = pixels90Sum / (float) pixelsSum;
+	}
+
+	caerLog(CAER_LOG_DEBUG, "AutoExposure",
+		"MaxNZBin: %zu, Bin10: %zu, Bin90: %zu, Sum: %zu, Sum10: %f, Sum90: %f, FracLow: %f, FracHigh: %f.",
+		state->pixelsMaxNonZeroBin, pixels10Bin, pixels90Bin, pixelsSum, (double) pixels10Sum, (double) pixels90Sum,
+		(double) pixelsFracLow, (double) pixelsFracHigh);
+
+	float pixelsError = (float) (pixelsMeanBin - (state->pixelsMaxNonZeroBin / 2)) / (float) state->pixelsMaxNonZeroBin;
 
 	// Exposure okay by default.
 	int32_t newExposure = -1;
 
-	if (meanSampleValueError > 0.1f) {
-		// Underexposed.
-		newExposure = I32T(exposureLastSetValue) + I32T(200.0f * meanSampleValueError * meanSampleValueError);
+	if ((pixelsFracLow >= AUTOEXPOSURE_UNDEROVER_FRAC) && (pixelsFracHigh < AUTOEXPOSURE_UNDEROVER_FRAC)) {
+		newExposure = I32T((float) exposureLastSetValue * (1.0f + AUTOEXPOSURE_CHANGE_FRAC));
 
-		// Clip exposure at maximum (1s = 1000000µs).
+		if (newExposure == I32T(exposureLastSetValue)) {
+			newExposure++; // Ensure increase.
+		}
 		if (newExposure > 1000000) {
 			newExposure = 1000000;
 		}
 	}
-	else if (meanSampleValueError < -0.1f) {
-		// Overexposed.
-		newExposure = I32T(exposureLastSetValue) - I32T(200.0f * meanSampleValueError * meanSampleValueError);
+	else if ((pixelsFracLow < AUTOEXPOSURE_UNDEROVER_FRAC) && (pixelsFracHigh >= AUTOEXPOSURE_UNDEROVER_FRAC)) {
+		newExposure = I32T((float) exposureLastSetValue * (1.0f - AUTOEXPOSURE_CHANGE_FRAC));
 
-		// Clip exposure at minimum (0µs).
+		if (newExposure == I32T(exposureLastSetValue)) {
+			newExposure--; // Ensure decrease.
+		}
 		if (newExposure < 0) {
 			newExposure = 0;
 		}
 	}
 
-	state->previousError = meanSampleValueError;
-	return (newExposure);
+	return ((newExposure == I32T(exposureLastSetValue)) ? (-1) : (newExposure));
 }
--- src/autoexposure.h
+++ src/autoexposure.h
@@ -4,11 +4,17 @@
 #include "libcaer.h"
 #include "events/frame.h"
 
-#define AUTOEXPOSURE_MIDDLEGRAY_MSV 5
+#define AUTOEXPOSURE_HISTOGRAM_PIXELS 256
+#define AUTOEXPOSURE_HISTOGRAM_MSV 5
+#define AUTOEXPOSURE_LOW_BOUNDARY 0.25f
+#define AUTOEXPOSURE_HIGH_BOUNDARY 0.75f
+#define AUTOEXPOSURE_UNDEROVER_FRAC 0.20f
+#define AUTOEXPOSURE_CHANGE_FRAC 0.10f
 
 struct auto_exposure_state {
-	size_t pixelHistogram[AUTOEXPOSURE_MIDDLEGRAY_MSV];
-	float previousError;
+	size_t pixelHistogram[AUTOEXPOSURE_HISTOGRAM_PIXELS];
+	size_t msvHistogram[AUTOEXPOSURE_HISTOGRAM_MSV];
+	size_t pixelsMaxNonZeroBin;
 };
 
 typedef struct auto_exposure_state *autoExposureState;
