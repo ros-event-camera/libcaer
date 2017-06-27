554ccb641f8567bf986630de431bca3c0f0113d4
federicohyo <federico.corradi@gmail.com>
2017-06-27 15:49:00 +0200

    enable davis aer control

---
+++ examples/davis_enable_aer.cpp
@@ -0,0 +1,74 @@
+#include <libcaercpp/devices/davis.hpp>
+#include <csignal>
+#include <atomic>
+
+using namespace std;
+
+static atomic_bool globalShutdown(false);
+
+static void globalShutdownSignalHandler(int signal) {
+	// Simply set the running flag to false on SIGTERM and SIGINT (CTRL+C) for global shutdown.
+	if (signal == SIGTERM || signal == SIGINT) {
+		globalShutdown.store(true);
+	}
+}
+
+int main(void) {
+	// Install signal handler for global shutdown.
+#if defined(_WIN32)
+	if (signal(SIGTERM, &globalShutdownSignalHandler) == SIG_ERR) {
+		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
+			"Failed to set signal handler for SIGTERM. Error: %d.", errno);
+		return (EXIT_FAILURE);
+	}
+
+	if (signal(SIGINT, &globalShutdownSignalHandler) == SIG_ERR) {
+		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
+			"Failed to set signal handler for SIGINT. Error: %d.", errno);
+		return (EXIT_FAILURE);
+	}
+#else
+	struct sigaction shutdownAction;
+
+	shutdownAction.sa_handler = &globalShutdownSignalHandler;
+	shutdownAction.sa_flags = 0;
+	sigemptyset(&shutdownAction.sa_mask);
+	sigaddset(&shutdownAction.sa_mask, SIGTERM);
+	sigaddset(&shutdownAction.sa_mask, SIGINT);
+
+	if (sigaction(SIGTERM, &shutdownAction, NULL) == -1) {
+		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
+			"Failed to set signal handler for SIGTERM. Error: %d.", errno);
+		return (EXIT_FAILURE);
+	}
+
+	if (sigaction(SIGINT, &shutdownAction, NULL) == -1) {
+		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
+			"Failed to set signal handler for SIGINT. Error: %d.", errno);
+		return (EXIT_FAILURE);
+	}
+#endif
+
+	// Open a DAVIS, give it a device ID of 1, and don't care about USB bus or SN restrictions.
+	libcaer::devices::davis davisHandle = libcaer::devices::davis(1);
+
+	// Let's take a look at the information we have on the device.
+	struct caer_davis_info davis_info = davisHandle.infoGet();
+
+	printf("%s --- ID: %d, Master: %d, DVS X: %d, DVS Y: %d, Logic: %d.\n", davis_info.deviceString,
+		davis_info.deviceID, davis_info.deviceIsMaster, davis_info.dvsSizeX, davis_info.dvsSizeY,
+		davis_info.logicVersion);
+
+	// Send the default configuration before using the device.
+	// No configuration is sent automatically!
+	davisHandle.sendDefaultConfig();
+
+	davisHandle.configSet(DAVIS_CONFIG_MUX, DAVIS_CONFIG_MUX_FORCE_CHIP_BIAS_ENABLE, true);
+	davisHandle.configSet(DAVIS_CONFIG_DVS, DAVIS_CONFIG_DVS_EXTERNAL_AER_CONTROL, true);
+
+	// Close automatically done by destructor.
+
+	printf("Shutdown successful.\n");
+
+	return (EXIT_SUCCESS);
+}
