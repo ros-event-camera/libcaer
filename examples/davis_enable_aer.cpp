2f3e4c8280243f631231120348f758cd923cd23b
Luca Longinotti <l@longi.li>
2017-06-29 16:00:04 +0200

    DAVIS AER external control example: cleanup code, remove signal handling, not needed.

--- examples/davis_enable_aer.cpp
+++ examples/davis_enable_aer.cpp
@@ -1,63 +1,18 @@
 /*
-* This c++ code shows how to open a DAVIS sensor,
+* This C++ code shows how to open a DAVIS sensor,
 * load the default biases, and enable the AER external control.
 *
-* After operating this routine, the program exit, leaving the davis sensor in AER mode.
+* After operating this routine, the program exits, leaving the DAVIS
+* sensor in external AER mode.
 *
 *  Author: Federico Corradi, 2017
 *  compile with: g++ -std=c++11 -pedantic -Wall -Wextra -O2 -o davis_enable_aer davis_enable_aer.cpp -D_DEFAULT_SOURCE=1 -lcaer
 */
 #include <libcaercpp/devices/davis.hpp>
-#include <csignal>
-#include <atomic>
 
 using namespace std;
 
-static atomic_bool globalShutdown(false);
-
-static void globalShutdownSignalHandler(int signal) {
-	// Simply set the running flag to false on SIGTERM and SIGINT (CTRL+C) for global shutdown.
-	if (signal == SIGTERM || signal == SIGINT) {
-		globalShutdown.store(true);
-	}
-}
-
 int main(void) {
-	// Install signal handler for global shutdown.
-#if defined(_WIN32)
-	if (signal(SIGTERM, &globalShutdownSignalHandler) == SIG_ERR) {
-		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
-			"Failed to set signal handler for SIGTERM. Error: %d.", errno);
-		return (EXIT_FAILURE);
-	}
-
-	if (signal(SIGINT, &globalShutdownSignalHandler) == SIG_ERR) {
-		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
-			"Failed to set signal handler for SIGINT. Error: %d.", errno);
-		return (EXIT_FAILURE);
-	}
-#else
-	struct sigaction shutdownAction;
-
-	shutdownAction.sa_handler = &globalShutdownSignalHandler;
-	shutdownAction.sa_flags = 0;
-	sigemptyset(&shutdownAction.sa_mask);
-	sigaddset(&shutdownAction.sa_mask, SIGTERM);
-	sigaddset(&shutdownAction.sa_mask, SIGINT);
-
-	if (sigaction(SIGTERM, &shutdownAction, NULL) == -1) {
-		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
-			"Failed to set signal handler for SIGTERM. Error: %d.", errno);
-		return (EXIT_FAILURE);
-	}
-
-	if (sigaction(SIGINT, &shutdownAction, NULL) == -1) {
-		libcaer::log::log(libcaer::log::logLevel::CRITICAL, "ShutdownAction",
-			"Failed to set signal handler for SIGINT. Error: %d.", errno);
-		return (EXIT_FAILURE);
-	}
-#endif
-
 	// Open a DAVIS, give it a device ID of 1, and don't care about USB bus or SN restrictions.
 	libcaer::devices::davis davisHandle = libcaer::devices::davis(1);
 
@@ -72,12 +27,11 @@ int main(void) {
 	// No configuration is sent automatically!
 	davisHandle.sendDefaultConfig();
 
+	// Enable external AER control, keep chip runnning.
 	davisHandle.configSet(DAVIS_CONFIG_MUX, DAVIS_CONFIG_MUX_FORCE_CHIP_BIAS_ENABLE, true);
 	davisHandle.configSet(DAVIS_CONFIG_DVS, DAVIS_CONFIG_DVS_EXTERNAL_AER_CONTROL, true);
 
 	// Close automatically done by destructor.
-
-	printf("Shutdown successful.\n");
 
 	return (EXIT_SUCCESS);
 }
