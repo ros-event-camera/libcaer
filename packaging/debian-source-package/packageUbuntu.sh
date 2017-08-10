8f50b383eb80589ad7fa0f59649d25cd410d114a
Luca Longinotti <l@longi.li>
2017-08-10 18:05:46 +0200

    Packaging: Debian scripts for artful, zesty and xenial distros.

--- packaging/debian-source-package/packageUbuntu.sh
+++ packaging/debian-source-package/packageUbuntu.sh
@@ -8,17 +8,20 @@ BRANCH=master
 DATE=$(LC_ALL=C date +'%a, %d %b %Y %T %z')
 CUR_DIR=$(pwd)
 BASE_DIR="$CUR_DIR/../../"
-BUILD_DIR="$CUR_DIR/$PKG_NAME/"
-DEBIAN_DIR="$BUILD_DIR/debian/"
-UPLOAD="true"
-DEBUILD_ARGS=""
+BUILD_DIR="$CUR_DIR/build/"
+PKG_BUILD_DIR="$BUILD_DIR/$PKG_NAME-$PKG_VERSION/"
+DEBIAN_DIR="$PKG_BUILD_DIR/debian/"
+UPLOAD="false"
+DEBUILD_ARGS="-k$GPG_PUBLIC_KEY -sa"
 
 while test $# -gt 0
 do
     case "$1" in
         --distro) DISTRO="$2"
             ;;
-        --no-upload) UPLOAD="false"
+        --source) DEBUILD_ARGS="-S $DEBUILD_ARGS"
+            ;;
+        --upload) UPLOAD="true"
             ;;
         --no-orig-tar-upload) DEBUILD_ARGS="$DEBUILD_ARGS -sd"
             ;;
@@ -28,7 +31,16 @@ done
 
 echo "Started the debian source packaging process for distro $DISTRO"
 
-rm -rf "$DEBIAN_DIR"
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+
+# Create the tar.gz containing the source and extract it
+cd "$BASE_DIR"
+git archive --format tar.gz --output "$BUILD_DIR/${PKG_NAME}_${PKG_VERSION}.orig.tar.gz" --prefix="$PKG_NAME-$PKG_VERSION/" "$BRANCH"
+
+cd "$BUILD_DIR"
+tar -xzf "${PKG_NAME}_${PKG_VERSION}.orig.tar.gz"
+
 mkdir -p "$DEBIAN_DIR"
 
 # Copy correct control and rules files for distro
@@ -38,26 +50,24 @@ cp "$CUR_DIR/$DISTRO/rules"   "$DEBIAN_DIR/rules"
 # Copy copyright file (use main license)
 cp "$BASE_DIR/COPYING" "$DEBIAN_DIR/copyright"
 
+# Formats file
+mkdir -p "$DEBIAN_DIR/source/"
+echo "3.0 (quilt)" > "$DEBIAN_DIR/source/format"
+
 # Create the changelog file for the distro
 CHANGELOG_FILE="$DEBIAN_DIR/changelog"
 echo "$PKG_NAME ($PKG_VERSION-0~$DISTRO) $DISTRO; urgency=low" > "$CHANGELOG_FILE"
 echo "" >> "$CHANGELOG_FILE"
-echo "  * Released $PKG_NAME version $PKG_VERSION." >> "$CHANGELOG_FILE"
+echo "  * Released $PKG_NAME version $PKG_VERSION for distro $DISTRO." >> "$CHANGELOG_FILE"
 echo "" >> "$CHANGELOG_FILE"
 echo " -- iniLabs <support@inilabs.com>  $DATE" >> "$CHANGELOG_FILE"
 
-# Create the tar.gz containing the source
-cd "$BASE_DIR"
-git archive --format tar.gz --output "$BUILD_DIR/${PKG_NAME}_${PKG_VERSION}.orig.tar.gz" --prefix=$PKG_NAME-$PKG_VERSION/ $BRANCH
-
-exit
-
 # Launch debuild
-cd "$BUILD_DIR"
-debuild -S -sa -k$GPG_PUBLIC_KEY $DEBUILD_ARGS
+cd "$PKG_BUILD_DIR"
+debuild $DEBUILD_ARGS
 
 # Send to Launchpad PPA
-if [[ "$UPLOAD" = "true" ]]; then
-	cd ..
+if [ "$UPLOAD" = "true" ]; then
+	cd "$BUILD_DIR"
 	dput ppa:llongi/inilabs "${PKG_NAME}_${PKG_VERSION}-0~${DISTRO}"_source.changes
 fi;
