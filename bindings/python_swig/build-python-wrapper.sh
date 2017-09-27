c3e4b05a37b5f08eddcc804d59bcb1c91f489264
Luca Longinotti <l@longi.li>
2017-09-27 19:32:39 +0200

    Python bindings: improve build script by automatically finding out libcaer and python locations via pkg-config.

--- bindings/python_swig/build-python-wrapper.sh
+++ bindings/python_swig/build-python-wrapper.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 # script that generates a python bindings of libcaer, it uses swig and gcc
-# check that your python version and adjust accordingly 
-swig -python -cpperraswarn -I/usr/local/include/ pyflags.i
-gcc -fPIC -c pyflags_wrap.c -I/usr/include/python2.7
+# check that your python version and adjust accordingly.
+
+swig -python -I$(pkg-config --variable=includedir libcaer) pyflags.i
+gcc -std=c11 -O2 -fPIC -c pyflags_wrap.c $(pkg-config --cflags python)
 ld -shared -lcaer pyflags_wrap.o -o _libcaer_wrap.so
