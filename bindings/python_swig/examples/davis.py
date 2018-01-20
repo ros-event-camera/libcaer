5eeb0d77fe79424892fd3d9a27a7a82527931ff6
Luca Longinotti <l@longi.li>
2018-01-20 19:29:14 +0100

    Python bindings: rewrite DVS128/DAVIS examples to be more similar to how we usually get data in a loop. Add examples to show how to generate accumulated event image, as well as display it and an eventual standard frame.

--- bindings/python_swig/examples/davis.py
+++ bindings/python_swig/examples/davis.py
@@ -1,41 +1,36 @@
 # Python example for interfacing with DAVIS
-# Author: federico.corradi@inilabs.com
+# Authors: federico.corradi@inilabs.com, luca.longinotti@inivation.com
 # basic example for python and libcaer
 # doc is here: https://inilabs.github.io/libcaer/
 
 import _libcaer_wrap as libcaer
-import numpy as np
+import numpy
+import cv2
+
 
 class DAVIS:
-    def __init__(self, busRestriction = 0, devAddressRestriction = 0, serialNumber = ""):
-        """ init DAVIS, display info, and start data transfer """   
+
+    def __init__(self, busRestriction=0, devAddressRestriction=0, serialNumber=""):
+        """ init DAVIS, display info, and start data transfer """
         self.handle = libcaer.caerDeviceOpen(1, libcaer.CAER_DEVICE_DAVIS, busRestriction, devAddressRestriction, serialNumber)
         self.info = libcaer.caerDavisInfoGet(self.handle)
-        print("device ID: "+str(libcaer.caer_davis_info_deviceID_get(self.info)))
-        if(libcaer.caer_davis_info_deviceIsMaster_get(self.info)):
-            print("device is Master: ")
+
+        print("device ID: " + str(libcaer.caer_davis_info_deviceID_get(self.info)))
+
+        if (libcaer.caer_davis_info_deviceIsMaster_get(self.info)):
+            print("device is Master")
         else:
             print("device is Slave")
-        print("device Serial Number"+str(libcaer.caer_davis_info_deviceSerialNumber_get(self.info)))	
+
+        print("device Serial Number: " + str(libcaer.caer_davis_info_deviceSerialNumber_get(self.info)))
         print(libcaer.caer_davis_info_deviceString_get(self.info))
-        
-        self.sizeX = libcaer.caer_davis_info_dvsSizeX_get(self.info)
-        self.sizeY = libcaer.caer_davis_info_dvsSizeY_get(self.info)
-        
-        # data containers
-        self.x = []
-        self.y = []
-        self.ts = []
-        self.pol = []
-        self.special = []
-        self.special_ts = []
-        self.frame = []
-        self.frame_ts = []
-        self.imu_gyro = []
-        self.imu_acc = []
-        self.imu_ts = []
-        self.imu_temp = []
-        
+
+        self.dvsSizeX = libcaer.caer_davis_info_dvsSizeX_get(self.info)
+        self.dvsSizeY = libcaer.caer_davis_info_dvsSizeY_get(self.info)
+
+        self.apsSizeX = libcaer.caer_davis_info_apsSizeX_get(self.info)
+        self.apsSizeY = libcaer.caer_davis_info_apsSizeY_get(self.info)
+
         # init default biases
         ret = libcaer.caerDeviceSendDefaultConfig(self.handle)
         if(ret == True):
@@ -43,98 +38,137 @@ class DAVIS:
         else:
             print("Error while loading default biases")
             raise Exception
-            
-        # start data transfer from device
-        ret = libcaer.caerDeviceDataStart(self.handle, None, None, None, None, None)
-        if(ret == True):
-            print("Data transfer started")
-        else:
-            print("Error in data transfer")
-            raise Exception 
-            
-        # set data exchange blocking    
+
+         # set blocking data exchange
         ret = libcaer.caerDeviceConfigSet(self.handle, libcaer.CAER_HOST_CONFIG_DATAEXCHANGE, libcaer.CAER_HOST_CONFIG_DATAEXCHANGE_BLOCKING, True)
         if(ret == True):
             print("Data exchange set to blocking mode")
         else:
             print("Error in communicating with the device, please check your setup")
-            raise Exception 
-        
-    def read_events(self):
-        """ A simple function that reads events from DAVIS sensors: polarity, imu6, special, frames"""
-        packetContainer = libcaer.caerDeviceDataGet(self.handle)
-        if packetContainer == None:
-            return False
+            raise Exception
+
+        # start data transfer from device
+        ret = libcaer.caerDeviceDataStart(self.handle, None, None, None, None, None)
+        if(ret == True):
+            print("Data transfer started")
         else:
+            print("Error in starting data transfer")
+            raise Exception
+
+    def read_events(self):
+        """ A simple function that reads events from DAVIS sensors: polarity, frame, imu, special"""
+        polarity = []
+        frame = []
+        imu = []
+        special = []
+
+        packetContainer = libcaer.caerDeviceDataGet(self.handle)
+
+        if packetContainer != None:
             packetNum = libcaer.caerEventPacketContainerGetEventPacketsNumber(packetContainer)
+
             for i in range(packetNum):
-                packetHeader = libcaer.caerEventPacketContainerGetEventPacket(packetContainer, i)
+                packetHeader = libcaer.caerEventPacketContainerGetEventPacketConst(packetContainer, i)
+
                 if packetHeader == None:
                     continue
+
                 packetType = libcaer.caerEventPacketHeaderGetEventType(packetHeader)
-                
-                #get only polarity and special events
+                eventNum = libcaer.caerEventPacketHeaderGetEventNumber(packetHeader)
+
                 if packetType == libcaer.POLARITY_EVENT:
-                    #loop over all polarity events
-                    nEvents = libcaer.caerEventPacketHeaderGetEventNumber(packetHeader)
-                    for this_e in range(nEvents):
-                        polarity = libcaer.caerPolarityEventPacketFromPacketHeader(packetHeader)
-                        event = libcaer.caerPolarityEventPacketGetEvent(polarity, this_e)
-                        self.ts.append(libcaer.caerPolarityEventGetTimestamp(event))
-                        self.x.append(libcaer.caerPolarityEventGetX(event))
-                        self.y.append(libcaer.caerPolarityEventGetY(event))
-                        self.pol.append(libcaer.caerPolarityEventGetPolarity(event))
-                        
-                if packetType == libcaer.SPECIAL_EVENT:
-                    #loop over all special events
-                    nEvents = libcaer.caerEventPacketHeaderGetEventNumber(packetHeader)
-                    for this_e in range(nEvents):
-                        special = libcaer.caerSpecialEventPacketFromPacketHeader(packetHeader)
-                        event = libcaer.caerSpecialEventPacketGetEvent(special, this_e)
-                        self.special_ts.append(libcaer.caerSpecialEventGetTimestamp(event))
-                        self.special.append(libcaer.caerSpecialEventGetData(event))
-                        
-                if packetType == libcaer.FRAME_EVENT:
-                    frame = libcaer.caerFrameEventPacketFromPacketHeader(packetHeader)
+                    # loop over all polarity events
+                    polarityPacket = libcaer.caerPolarityEventPacketFromPacketHeaderConst(packetHeader)
+
+                    for e in range(eventNum):
+                        polarityEvent = libcaer.caerPolarityEventPacketGetEventConst(polarityPacket, e)
+
+                        polarity_ts = libcaer.caerPolarityEventGetTimestamp(polarityEvent)
+                        polarity_x = libcaer.caerPolarityEventGetX(polarityEvent)
+                        polarity_y = libcaer.caerPolarityEventGetY(polarityEvent)
+                        polarity_pol = libcaer.caerPolarityEventGetPolarity(polarityEvent)
+
+                        polarity.append((polarity_ts, polarity_x, polarity_y, polarity_pol))
+
+                elif packetType == libcaer.SPECIAL_EVENT:
+                    # loop over all special events
+                    specialPacket = libcaer.caerSpecialEventPacketFromPacketHeaderConst(packetHeader)
+
+                    for e in range(eventNum):
+                        specialEvent = libcaer.caerSpecialEventPacketGetEventConst(specialPacket, e)
+
+                        special_ts = libcaer.caerSpecialEventGetTimestamp(specialEvent)
+                        special_type = libcaer.caerSpecialEventGetType(specialEvent)
+
+                        special.append((special_ts, special_type))
+
+                elif packetType == libcaer.FRAME_EVENT:
                     # only get first frame event in packet
-                    firstEvent = libcaer.caerFrameEventPacketGetEventConst(frame, 0)
-                    self.frame_ts = libcaer.caerFrameEventGetTimestamp(firstEvent)
-                    matrix_frame = np.zeros([libcaer.caer_davis_info_dvsSizeX_get(self.info), libcaer.caer_davis_info_dvsSizeY_get(self.info)])
-                    #read pixels values
-                    for y in range(libcaer.caerFrameEventGetLengthY(firstEvent)):
-                        for x in range(libcaer.caerFrameEventGetLengthX(firstEvent)):
-                            matrix_frame[x,y] = libcaer.caerFrameEventGetPixel(firstEvent, x, y)
-                    self.frame.append(matrix_frame)
-                    
-                if packetType == libcaer.IMU6_EVENT:    
-                    nEvents = libcaer.caerEventPacketHeaderGetEventNumber(packetHeader)
-                    imu = libcaer.caerIMU6EventPacketFromPacketHeader(packetHeader)
-                    for this_e in range(nEvents):
-                        imu6 = libcaer.caerIMU6EventPacketGetEvent(imu, this_e)
-                        x_acc = libcaer.caerIMU6EventGetAccelX(imu6)
-                        y_acc = libcaer.caerIMU6EventGetAccelY(imu6)
-                        z_acc = libcaer.caerIMU6EventGetAccelZ(imu6)
-                        x_gyro = libcaer.caerIMU6EventGetGyroX(imu6)
-                        y_gyro = libcaer.caerIMU6EventGetGyroY(imu6)
-                        z_gyro = libcaer.caerIMU6EventGetGyroZ(imu6)
-                        self.imu_acc.append([x_acc, y_acc, z_acc])
-                        self.imu_gyro.append([x_gyro, y_gyro, z_gyro])
-                        self.imu_ts.append(libcaer.caerIMU6EventGetTimestamp(imu6))
-                        self.imu_temp.append(libcaer.caerIMU6EventGetTemp(imu6))                        
-        return True
-        
+                    framePacket = libcaer.caerFrameEventPacketFromPacketHeaderConst(packetHeader)
+
+                    frameEvent = libcaer.caerFrameEventPacketGetEventConst(framePacket, 0)
+
+                    frame_numpy = numpy.zeros((self.apsSizeY, self.apsSizeX), dtype=numpy.uint16)
+
+                    # read pixels values
+                    for y in range(libcaer.caerFrameEventGetLengthY(frameEvent)):
+                        for x in range(libcaer.caerFrameEventGetLengthX(frameEvent)):
+                            frame_numpy[y, x] = libcaer.caerFrameEventGetPixel(frameEvent, x, y)
+
+                    frame_ts = libcaer.caerFrameEventGetTimestamp(frameEvent)
+
+                    frame.append((frame_ts, frame_numpy))
+
+                elif packetType == libcaer.IMU6_EVENT:
+                    # loop over all IMU 6-axis events
+                    imuPacket = libcaer.caerIMU6EventPacketFromPacketHeaderConst(packetHeader)
+
+                    for e in range(eventNum):
+                        imuEvent = libcaer.caerIMU6EventPacketGetEventConst(imuPacket, e)
+
+                        x_acc = libcaer.caerIMU6EventGetAccelX(imuEvent)
+                        y_acc = libcaer.caerIMU6EventGetAccelY(imuEvent)
+                        z_acc = libcaer.caerIMU6EventGetAccelZ(imuEvent)
+                        x_gyro = libcaer.caerIMU6EventGetGyroX(imuEvent)
+                        y_gyro = libcaer.caerIMU6EventGetGyroY(imuEvent)
+                        z_gyro = libcaer.caerIMU6EventGetGyroZ(imuEvent)
+
+                        imu_ts = libcaer.caerIMU6EventGetTimestamp(imuEvent)
+                        imu_acc = (x_acc, y_acc, z_acc)
+                        imu_gyro = (x_gyro, y_gyro, z_gyro)
+                        imu_temp = libcaer.caerIMU6EventGetTemp(imuEvent)
+
+                        imu.append((imu_ts, imu_acc, imu_gyro, imu_temp))
+
+        return polarity, frame, imu, special
+
+
 if __name__ == "__main__":
+    camera = DAVIS()
 
-    import davis
+    try:
+        while True:
+            polarity, frame, imu, special = camera.read_events()
 
-    camera = davis.DAVIS()
-    if(camera.read_events() == True):
-        print("succesfully read events from the device")
-        print("pixels x:"+str(camera.x))
-        print("pixels y:"+str(camera.y)) 
-        print("pixels ts:"+str(camera.ts))  
-        print("pixels pol:"+str(camera.pol)) 
-        print("camera frames:"+str(len(camera.frame))) 
-    else:
-        print("nothing read from the device")     
-    
+            # if there are polarity events, accumulate them into a numpy array
+            # and display it (black/white coding)
+            if len(polarity) > 0:
+                matrix_events = numpy.full((camera.dvsSizeY, camera.dvsSizeX), 0.5)
+
+                for evt in polarity:
+                    matrix_events[evt[2], evt[1]] = evt[3]
+
+                cv2.imshow("polarity", matrix_events)
+
+            # if a standard frame exists, show it
+            if len(frame) > 0:
+                cv2.imshow("frame", frame[0][1])
+
+            # wait 1ms on user input, required for OpenCV imshow()
+            if len(polarity) > 0 or len(frame) > 0:
+                cv2.waitKey(1)
+
+    except KeyboardInterrupt:
+        # close camera on CTRL+C
+        libcaer.caerDeviceDataStop(camera.handle)
+        libcaer.caerDeviceClose(camera.handle)
--- bindings/python_swig/examples/dvs128.py
+++ bindings/python_swig/examples/dvs128.py
@@ -1,31 +1,33 @@
 # Python example for interfacing with DVS128
-# Author: federico.corradi@inilabs.com
+# Authors: federico.corradi@inilabs.com, luca.longinotti@inivation.com
 # basic example for python and libcaer
-# online doc is here : https://inilabs.github.io/libcaer/files.html
+# doc is here: https://inilabs.github.io/libcaer/
 
 import _libcaer_wrap as libcaer
+import numpy
+import cv2
+
 
 class DVS128:
-    def __init__(self, busRestriction = 0, devAddressRestriction = 0, serialNumber = ""):
-        """ init DVS128, display info, and start data transfer """   
+
+    def __init__(self, busRestriction=0, devAddressRestriction=0, serialNumber=""):
+        """ init DVS128, display info, and start data transfer """
         self.handle = libcaer.caerDeviceOpen(1, libcaer.CAER_DEVICE_DVS128, busRestriction, devAddressRestriction, serialNumber)
         self.info = libcaer.caerDVS128InfoGet(self.handle)
-        print("device ID: "+str(libcaer.caer_dvs128_info_deviceID_get(self.info)))
-        if(libcaer.caer_dvs128_info_deviceIsMaster_get(self.info)):
-            print("device is Master: ")
+
+        print("device ID: " + str(libcaer.caer_dvs128_info_deviceID_get(self.info)))
+
+        if (libcaer.caer_dvs128_info_deviceIsMaster_get(self.info)):
+            print("device is Master")
         else:
             print("device is Slave")
-        print("device Serial Number"+str(libcaer.caer_dvs128_info_deviceSerialNumber_get(self.info)))	
+
+        print("device Serial Number: " + str(libcaer.caer_dvs128_info_deviceSerialNumber_get(self.info)))
         print(libcaer.caer_dvs128_info_deviceString_get(self.info))
-        
-        # containers
-        self.x = []
-        self.y = []
-        self.ts = []
-        self.pol = []
-        self.special = []
-        self.special_ts = []
-        
+
+        self.dvsSizeX = libcaer.caer_dvs128_info_dvsSizeX_get(self.info)
+        self.dvsSizeY = libcaer.caer_dvs128_info_dvsSizeY_get(self.info)
+
         # init default biases
         ret = libcaer.caerDeviceSendDefaultConfig(self.handle)
         if(ret == True):
@@ -33,62 +35,90 @@ class DVS128:
         else:
             print("Error while loading default biases")
             raise Exception
-            
-        # start data transfer from device
-        ret = libcaer.caerDeviceDataStart(self.handle, None, None, None, None, None)
-        if(ret == True):
-            print("Data transfer started")
-        else:
-            print("Error in data transfer")
-            raise Exception 
-            
-        # set data exchange blocking    
+
+         # set blocking data exchange
         ret = libcaer.caerDeviceConfigSet(self.handle, libcaer.CAER_HOST_CONFIG_DATAEXCHANGE, libcaer.CAER_HOST_CONFIG_DATAEXCHANGE_BLOCKING, True)
         if(ret == True):
             print("Data exchange set to blocking mode")
         else:
             print("Error in communicating with the device, please check your setup")
-            raise Exception 
-        
+            raise Exception
+
+        # start data transfer from device
+        ret = libcaer.caerDeviceDataStart(self.handle, None, None, None, None, None)
+        if(ret == True):
+            print("Data transfer started")
+        else:
+            print("Error in starting data transfer")
+            raise Exception
+
     def read_events(self):
-        """ A simple function that reads events from dvs128 """
+        """ A simple function that reads events from DVS128 sensors: polarity, special"""
+        polarity = []
+        special = []
+
         packetContainer = libcaer.caerDeviceDataGet(self.handle)
-        packetNum = libcaer.caerEventPacketContainerGetEventPacketsNumber(packetContainer)
-        for i in range(packetNum):
-            packetHeader = libcaer.caerEventPacketContainerGetEventPacket(packetContainer, i)
-            if packetHeader == None:
-                continue
-            packetType = libcaer.caerEventPacketHeaderGetEventType(packetHeader)
-            
-            #get only polarity and special events
-            if packetType == libcaer.POLARITY_EVENT:
-                #loop over all events
-                nEvents = libcaer.caerEventPacketHeaderGetEventNumber(packetHeader)
-                for this_e in range(nEvents):
-                    polarity = libcaer.caerPolarityEventPacketFromPacketHeader(packetHeader)
-                    event = libcaer.caerPolarityEventPacketGetEvent(polarity, this_e)
-                    self.ts.append(libcaer.caerPolarityEventGetTimestamp(event))
-                    self.x.append(libcaer.caerPolarityEventGetX(event))
-                    self.y.append(libcaer.caerPolarityEventGetY(event))
-                    self.pol.append(libcaer.caerPolarityEventGetPolarity(event))
-            if packetType == libcaer.SPECIAL_EVENT:
-                #loop over all special events
-                nEvents = libcaer.caerEventPacketHeaderGetEventNumber(packetHeader)
-                for this_e in range(nEvents):
-                    polarity = libcaer.caerSpecialEventPacketFromPacketHeader(packetHeader)
-                    event = libcaer.caerPolarityEventPacketGetEvent(polarity, this_e)
-                    self.special_ts.append(libcaer.caerSpecialEventGetTimestamp(event))
-                    self.special.append(libcaer.caerSpecialEventGetData(event))
-             
-        
+
+        if packetContainer != None:
+            packetNum = libcaer.caerEventPacketContainerGetEventPacketsNumber(packetContainer)
+
+            for i in range(packetNum):
+                packetHeader = libcaer.caerEventPacketContainerGetEventPacketConst(packetContainer, i)
+
+                if packetHeader == None:
+                    continue
+
+                packetType = libcaer.caerEventPacketHeaderGetEventType(packetHeader)
+                eventNum = libcaer.caerEventPacketHeaderGetEventNumber(packetHeader)
+
+                if packetType == libcaer.POLARITY_EVENT:
+                    # loop over all polarity events
+                    polarityPacket = libcaer.caerPolarityEventPacketFromPacketHeaderConst(packetHeader)
+
+                    for e in range(eventNum):
+                        polarityEvent = libcaer.caerPolarityEventPacketGetEventConst(polarityPacket, e)
+
+                        polarity_ts = libcaer.caerPolarityEventGetTimestamp(polarityEvent)
+                        polarity_x = libcaer.caerPolarityEventGetX(polarityEvent)
+                        polarity_y = libcaer.caerPolarityEventGetY(polarityEvent)
+                        polarity_pol = libcaer.caerPolarityEventGetPolarity(polarityEvent)
+
+                        polarity.append((polarity_ts, polarity_x, polarity_y, polarity_pol))
+
+                elif packetType == libcaer.SPECIAL_EVENT:
+                    # loop over all special events
+                    specialPacket = libcaer.caerSpecialEventPacketFromPacketHeaderConst(packetHeader)
+
+                    for e in range(eventNum):
+                        specialEvent = libcaer.caerSpecialEventPacketGetEventConst(specialPacket, e)
+
+                        special_ts = libcaer.caerSpecialEventGetTimestamp(specialEvent)
+                        special_type = libcaer.caerSpecialEventGetType(specialEvent)
+
+                        special.append((special_ts, special_type))
+
+        return polarity, special
+
+
 if __name__ == "__main__":
+    camera = DVS128()
 
-    import dvs128
+    try:
+        while True:
+            polarity, special = camera.read_events()
 
-    camera = dvs128.DVS128()
-    camera.read_events()
-    print("pixels x:"+str(camera.x))
-    print("pixels y:"+str(camera.y)) 
-    print("pixels ts:"+str(camera.ts))  
-    print("pixels pol:"+str(camera.pol))  
-    
+            # if there are polarity events, accumulate them into a numpy array
+            # and display it (black/white coding)
+            if len(polarity) > 0:
+                matrix_events = numpy.full((camera.dvsSizeY, camera.dvsSizeX), 0.5)
+
+                for evt in polarity:
+                    matrix_events[evt[2], evt[1]] = evt[3]
+
+                cv2.imshow("polarity", matrix_events)
+                cv2.waitKey(1)
+
+    except KeyboardInterrupt:
+        # close camera on CTRL+C
+        libcaer.caerDeviceDataStop(camera.handle)
+        libcaer.caerDeviceClose(camera.handle)
