b69585138ef1162f800179ffc1e54e5face437c8
Luca Longinotti <luca.longinotti@inivation.com>
2019-03-26 15:39:20 +0100

    C++ RingBuffer: full C++ implementation of ring-buffer via templates.

--- includecpp/ringbuffer.hpp
+++ includecpp/ringbuffer.hpp
@@ -1,64 +1,112 @@
 #ifndef LIBCAER_RINGBUFFER_HPP_
 #define LIBCAER_RINGBUFFER_HPP_
 
-#include <libcaer/ringbuffer.h>
-
-#include <memory>
+#include <atomic>
+#include <cstdint>
 #include <stdexcept>
+#include <vector>
+
+// Alignment specification support (with defines for cache line alignment).
+#if !defined(CACHELINE_SIZE)
+#	define CACHELINE_SIZE 64 // Default (big enough for most processors), must be power of two!
+#endif
 
 namespace libcaer {
 namespace ringbuffer {
 
-class RingBuffer {
+template<typename T> class RingBuffer {
 private:
-	std::shared_ptr<struct caer_ring_buffer> ringBuffer;
+	alignas(CACHELINE_SIZE) size_t putPos;
+	alignas(CACHELINE_SIZE) size_t getPos;
+	alignas(CACHELINE_SIZE) const std::vector<std::atomic<T>> elements;
+	const size_t sizeAdj;
+	const T placeholder;
 
 public:
-	RingBuffer(size_t size) {
-		caerRingBuffer rBuf = caerRingBufferInit(size);
-
-		// Handle constructor failure.
-		if (rBuf == nullptr) {
-			throw std::runtime_error("Failed to initialize ring-buffer.");
+	RingBuffer(size_t sz) : putPos(0), getPos(0), elements(sz), sizeAdj(sz - 1), placeholder() {
+		// Force multiple of two size for performance.
+		if ((sz == 0) || ((sz & sizeAdj) != 0)) {
+			throw std::invalid_argument("Size must be a power of two.");
 		}
-
-		// Use stateless lambda for shared_ptr custom deleter.
-		auto deleteRingBuffer = [](caerRingBuffer rbf) {
-			// Run destructor, free all memory.
-			// Never fails in current implementation.
-			caerRingBufferFree(rbf);
-		};
-
-		ringBuffer = std::shared_ptr<struct caer_ring_buffer>(rBuf, deleteRingBuffer);
 	}
 
 	bool operator==(const RingBuffer &rhs) const noexcept {
-		return (ringBuffer == rhs.ringBuffer);
+		return (this == &rhs);
 	}
 
 	bool operator!=(const RingBuffer &rhs) const noexcept {
 		return (!operator==(rhs));
 	}
 
-	void put(void *elem) const {
-		bool success = caerRingBufferPut(ringBuffer.get(), elem);
-		if (!success) {
-			throw std::runtime_error("Failed to put element on ring-buffer.");
+	void put(const T &elem) {
+		if (elem == placeholder) {
+			// Default constructed elements are disallowed (used as place-holders).
+			throw std::invalid_argument("Default constructed elements are not allowed in the ringbuffer.");
 		}
+
+		const T &curr = elements[putPos].load(std::memory_order_acquire);
+
+		// If the place where we want to put the new element is NULL, it's still
+		// free and we can use it.
+		if (curr == placeholder) {
+			elements[putPos].store(elem, std::memory_order_release);
+
+			// Increase local put pointer.
+			putPos = ((putPos + 1) & sizeAdj);
+
+			return;
+		}
+
+		// Else, buffer is full.
+		throw std::out_of_range("Ringbuffer full.");
 	}
 
 	bool full() const noexcept {
-		return (caerRingBufferFull(ringBuffer.get()));
+		const T &curr = elements[putPos].load(std::memory_order_acquire);
+
+		// If the place where we want to put the new element is NULL, it's still
+		// free and thus the buffer still has available space.
+		if (curr == placeholder) {
+			return (false);
+		}
+
+		// Else, buffer is full.
+		return (true);
 	}
 
-	void *get() const noexcept {
-		return (caerRingBufferGet(ringBuffer.get()));
+	T get() {
+		T curr = elements[getPos].load(std::memory_order_acquire);
+
+		// If the place where we want to get an element from is not NULL, there
+		// is valid content there, which we return, and reset the place to NULL.
+		if (curr != placeholder) {
+			elements[getPos].store(placeholder, std::memory_order_release);
+
+			// Increase local get pointer.
+			getPos = ((getPos + 1) & sizeAdj);
+
+			return (curr);
+		}
+
+		// Else, buffer is empty.
+		throw std::out_of_range("Ringbuffer empty.");
 	}
 
-	void *look() const noexcept {
-		return (caerRingBufferLook(ringBuffer.get()));
+	T look() const {
+		T curr = elements[getPos].load(std::memory_order_acquire);
+
+		// If the place where we want to get an element from is not NULL, there
+		// is valid content there, which we return, without removing it from the
+		// ring buffer.
+		if (curr != placeholder) {
+			return (curr);
+		}
+
+		// Else, buffer is empty.
+		throw std::out_of_range("Ringbuffer empty.");
 	}
 };
+
 } // namespace ringbuffer
 } // namespace libcaer
 
