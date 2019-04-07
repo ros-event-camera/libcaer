c007dcd07f8fbd7e0f2ac58526db61578f40eb19
Luca Longinotti <l@longi.li>
2019-04-07 17:44:18 +0200

    DVS Noise filter: fix macro.

--- src/filters_dvs_noise.c
+++ src/filters_dvs_noise.c
@@ -43,7 +43,7 @@ struct dvs_pixel_with_count {
 
 #define GET_TS(X) ((X) >> 1)
 #define GET_POL(X) ((X) &0x01)
-#define SET_TSPOL(TS, POL) (((TS) << 1) | (pol & 0x01))
+#define SET_TSPOL(TS, POL) (((TS) << 1) | ((POL) &0x01))
 
 static void filterDVSNoiseLog(enum caer_log_level logLevel, caerFilterDVSNoise handle, const char *format, ...)
 	ATTRIBUTE_FORMAT(3);
