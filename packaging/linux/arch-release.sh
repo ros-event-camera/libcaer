cd06e457dc523b65fc6333db10327830d0e2c8a1
Luca Longinotti <luca.longinotti@inivation.com>
2019-12-03 15:21:33 +0100

    Fix new group URLs.

--- packaging/linux/arch-release.sh
+++ packaging/linux/arch-release.sh
@@ -18,7 +18,7 @@ PKG_FILE="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_FILE_PATH="/var/cache/distfiles/${PKG_FILE}"
 
 if ! [ -f "${PKG_FILE_PATH}" ] ; then
-	wget "https://gitlab.com/inivation/${PKG_NAME}/-/archive/${PKG_VERSION}/${PKG_FILE}" -O "${PKG_FILE_PATH}"
+	wget "https://gitlab.com/inivation/dv/${PKG_NAME}/-/archive/${PKG_VERSION}/${PKG_FILE}" -O "${PKG_FILE_PATH}"
 fi
 
 SHA256_FILE=$(sha256sum "${PKG_FILE_PATH}" | awk '{ print $1 }')
